3d569aa5d4ca027456f0c9045846e1bd24415e43
Łukasz Łapaj <4143729+LukaszLapaj@users.noreply.github.com>
2021-01-12 16:51:06 +0100

    Add class not-draggable

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ import "./Header.css";
 export const Header = () => (
     <>
         <div>
-            <img className="img-fluid mx-auto d-block" loading="eager" width="398" height="398" alt="" src="https://avatars0.githubusercontent.com/u/4143729?s=460&v=5" />
+            <img className="img-fluid mx-auto d-block not-draggable" loading="eager" width="398" height="398" alt="" src="https://avatars0.githubusercontent.com/u/4143729?s=460&v=5" />
         </div>
         <div>
             <div className="mt-2 mb-2">
@@ -19,11 +19,11 @@ export const Header = () => (
         </div>
         <div>
             <div className="d-flex justify-content-between align-content-center my-3 px-4">
-                <a href="https://www.linkedin.com/in/lukaszlapaj/" target="_blank" rel="noreferrer"><img alt="LI" className="ll-social" src={LinkedIn} /></a>
-                <a href="https://github.com/lukaszlapaj" target="_blank" rel="noreferrer"><img alt="GH" className="ll-social" src={GitHub} /></a>
-                <a href="mailto:lukaszlapaj@interia.pl" target="_blank" rel="noreferrer"><img alt="MAIL" className="ll-social" src={Mail} /></a>
-                <a href="https://www.facebook.com/LukaszLapaj" target="_blank" rel="noreferrer"><img alt="FB" className="ll-social" src={Facebook} /></a>
-                <a href="https://www.instagram.com/eloeloou" target="_blank" rel="noreferrer"><img alt="IG" className="ll-social" src={Instagram} /></a>
+                <a className="not-draggable" href="https://www.linkedin.com/in/lukaszlapaj/" target="_blank" rel="noreferrer"><img alt="LI" className="ll-social" src={LinkedIn} /></a>
+                <a className="not-draggable" href="https://github.com/lukaszlapaj" target="_blank" rel="noreferrer"><img alt="GH" className="ll-social" src={GitHub} /></a>
+                <a className="not-draggable" href="mailto:lukaszlapaj@interia.pl" target="_blank" rel="noreferrer"><img alt="MAIL" className="ll-social" src={Mail} /></a>
+                <a className="not-draggable" href="https://www.facebook.com/LukaszLapaj" target="_blank" rel="noreferrer"><img alt="FB" className="ll-social" src={Facebook} /></a>
+                <a className="not-draggable" href="https://www.instagram.com/eloeloou" target="_blank" rel="noreferrer"><img alt="IG" className="ll-social" src={Instagram} /></a>
             </div>
         </div>
     </>
