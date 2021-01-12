ab6e9e9cb51f48b39abfcf0d959d99e2b7b6b6fa
Łukasz Łapaj <4143729+LukaszLapaj@users.noreply.github.com>
2021-01-12 20:34:00 +0100

    Fix routing to CV

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,10 +6,10 @@ import Pdf from '../../assets/CV.pdf';
 const Projects = () => (
     <div>
         <div className="d-flex justify-content-between align-content-center mt-2 mb-3 px-1">
-            <NavLink to="/" className="ll-link nav-link not-draggable" activeClassName="active">Me</NavLink>
-            <NavLink to={Pdf} target="_blank" className="nav-link ll-link not-draggable" activeClassName="active">CV</NavLink>
-            <NavLink to="/projects" className="nav-link ll-link not-draggable" activeClassName="">Projects</NavLink>
-            <NavLink to="/technologies" className="nav-link ll-link not-draggable" activeClassName="active">Technologies</NavLink>
+            <NavLink exact to="/" className="ll-link nav-link not-draggable">Me</NavLink>
+            <a href={Pdf} className="nav-link ll-link not-draggable" target="_blank" rel="noreferrer">CV</a>
+            <NavLink to="/projects" className="nav-link ll-link not-draggable">Projects</NavLink>
+            <NavLink to="/technologies" className="nav-link ll-link not-draggable">Technologies</NavLink>
         </div>
     </div>
 );
