4f4861a3cdd3c5c663cb8523743c747e62b1832a
Łukasz Łapaj <4143729+LukaszLapaj@users.noreply.github.com>
2021-04-23 15:45:28 +0200

    Move CV to another repository

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Navigation.css'
 import {NavLink} from "react-router-dom";
-import Pdf from '../../assets/CV.pdf';
 
 const Projects = () => (
     <div>
         <div className="d-flex justify-content-between align-content-center mt-2 mb-3 px-1">
             <NavLink exact to="/" className="ll-link nav-link not-draggable">Me</NavLink>
-            <a href={Pdf} className="nav-link ll-link not-draggable" target="_blank" rel="noreferrer">CV</a>
+            <a href={"https://lukaszlapaj.github.io/CV/Lukasz_Lapaj_CV.pdf"} className="nav-link ll-link not-draggable" target="_blank" rel="noreferrer">CV</a>
             <NavLink to="/projects" className="nav-link ll-link not-draggable">Projects</NavLink>
             <NavLink to="/technologies" className="nav-link ll-link not-draggable">Technologies</NavLink>
         </div>
