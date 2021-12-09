a33ee9e694ced604d69adc9bcfd6a9180c937738
Łukasz Łapaj <4143729+LukaszLapaj@users.noreply.github.com>
2021-12-09 14:10:25 +0100

    Update about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,8 +6,8 @@ const About = () => (
     <div className="ll-about">
         <span className="text-justify">
             <p>Hi Friend.</p>
-            <p>My name is Łukasz. I am a Computer Science Engineer and student of the Information Technology and Econometrics.</p>
-            <p>Currently taking part in the internship organized at ABB Innovation & Emerging Services in a role of Software Quality Engineer.
+            <p>My name is Łukasz. I am a Computer Science Engineer and Bachelor of Science in Information Technology and Econometrics.</p>
+            <p>Currently working at ABB GBS IS, Innovation & Emerging Services in a role of Software Quality Assurance Specialist.
                 Focusing on the test automation of cognitive services WebAPIs, creating test cases and test scenarios, performance testing, and related tasks.</p>
             <p>Personally interested in Software Development and Testing, but not closed for other directions.</p>
             <p>Constantly looking for new challenges and opportunities.</p>
