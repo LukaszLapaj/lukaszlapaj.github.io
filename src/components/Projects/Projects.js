d2ccf528aae7e662e0c9d74646a979ff71233448
Łukasz Łapaj <4143729+LukaszLapaj@users.noreply.github.com>
2022-04-08 19:03:47 +0200

    Update to React 18 & Bootstrap 5

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,27 +4,27 @@ import "./Projects.css"
 
 const Projects = () => (
     <div className="ll-projects">
-        <p className="font-weight-bold h3">Projects</p>
-        <p><span className="font-weight-bold">AirPurifierDriver:</span><br/>
-            <a className="github-url" href="https://github.com/LukaszLapaj/AirPurifierDriver" target="_blank" rel="noreferrer">github.com/LukaszLapaj/AirPurifierDriver</a></p>
-        <p><span className="font-weight-bold">AirCheck:</span><br/>
-            <a className="github-url" href="https://github.com/LukaszLapaj/AirCheck" target="_blank" rel="noreferrer">github.com/LukaszLapaj/AirCheck</a></p>
-        <p><span className="font-weight-bold">software-testing-resource-pack:</span><br/>
-            <a className="github-url" href="https://github.com/LukaszLapaj/software-testing-resource-pack" target="_blank" rel="noreferrer">github.com/LukaszLapaj/software-testing-resource-pack</a></p>
-        <p><span className="font-weight-bold">raspberry-pi-cheat-sheet:</span><br/>
-            <a className="github-url" href="https://github.com/LukaszLapaj/raspberry-pi-cheat-sheet" target="_blank" rel="noreferrer">github.com/LukaszLapaj/raspberry-pi-cheat-sheet</a></p>
-        <p><span className="font-weight-bold">project-setups:</span><br/>
-            <a className="github-url" href="https://github.com/LukaszLapaj/project-setups" target="_blank" rel="noreferrer">github.com/LukaszLapaj/project-setups</a></p>
-        <p><span className="font-weight-bold">GoogleTaskConverter:</span><br/>
-            <a className="github-url" href="https://github.com/LukaszLapaj/GoogleTaskConverter" target="_blank" rel="noreferrer">github.com/LukaszLapaj/GoogleTaskConverter</a></p>
-        <p><span className="font-weight-bold">SHSH2-Helper:</span><br/>
-            <a className="github-url" href="https://github.com/LukaszLapaj/SHSH2-Helper" target="_blank" rel="noreferrer">github.com/LukaszLapaj/SHSH2-Helper</a></p>
+        <p className="fw-bold h3">Projects</p>
+        <p><span className="fw-bold">AirPurifierDriver:</span><br/>
+            <a className="github-url text-decoration-none" href="https://github.com/LukaszLapaj/AirPurifierDriver" target="_blank" rel="noreferrer">github.com/LukaszLapaj/AirPurifierDriver</a></p>
+        <p><span className="fw-bold">AirCheck:</span><br/>
+            <a className="github-url text-decoration-none" href="https://github.com/LukaszLapaj/AirCheck" target="_blank" rel="noreferrer">github.com/LukaszLapaj/AirCheck</a></p>
+        <p><span className="fw-bold">software-testing-resource-pack:</span><br/>
+            <a className="github-url text-decoration-none" href="https://github.com/LukaszLapaj/software-testing-resource-pack" target="_blank" rel="noreferrer">github.com/LukaszLapaj/software-testing-resource-pack</a></p>
+        <p><span className="fw-bold">raspberry-pi-cheat-sheet:</span><br/>
+            <a className="github-url text-decoration-none" href="https://github.com/LukaszLapaj/raspberry-pi-cheat-sheet" target="_blank" rel="noreferrer">github.com/LukaszLapaj/raspberry-pi-cheat-sheet</a></p>
+        <p><span className="fw-bold">project-setups:</span><br/>
+            <a className="github-url text-decoration-none" href="https://github.com/LukaszLapaj/project-setups" target="_blank" rel="noreferrer">github.com/LukaszLapaj/project-setups</a></p>
+        <p><span className="fw-bold">GoogleTaskConverter:</span><br/>
+            <a className="github-url text-decoration-none" href="https://github.com/LukaszLapaj/GoogleTaskConverter" target="_blank" rel="noreferrer">github.com/LukaszLapaj/GoogleTaskConverter</a></p>
+        <p><span className="fw-bold">SHSH2-Helper:</span><br/>
+            <a className="github-url text-decoration-none" href="https://github.com/LukaszLapaj/SHSH2-Helper" target="_blank" rel="noreferrer">github.com/LukaszLapaj/SHSH2-Helper</a></p>
         <br/>
-        <p className="font-weight-bold h3">Contributions / Forks</p>
-        <p><span className="font-weight-bold">DisableWinTracking:</span><br/>
-            <a className="github-url" href="https://github.com/LukaszLapaj/DisableWinTracking" target="_blank" rel="noreferrer">github.com/LukaszLapaj/DisableWinTracking</a></p>
-        <p><span className="font-weight-bold">miio:</span><br/>
-            <a className="github-url" href="https://github.com/LukaszLapaj/miio" target="_blank" rel="noreferrer">github.com/LukaszLapaj/miio</a></p>
+        <p className="fw-bold h3">Contributions / Forks</p>
+        <p><span className="fw-bold">DisableWinTracking:</span><br/>
+            <a className="github-url text-decoration-none" href="https://github.com/LukaszLapaj/DisableWinTracking" target="_blank" rel="noreferrer">github.com/LukaszLapaj/DisableWinTracking</a></p>
+        <p><span className="fw-bold">miio:</span><br/>
+            <a className="github-url text-decoration-none" href="https://github.com/LukaszLapaj/miio" target="_blank" rel="noreferrer">github.com/LukaszLapaj/miio</a></p>
     </div>
 );
 
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -5,16 +5,16 @@ import "./Technologies.css"
 const Technologies = () => (
     <div className="ll-technologies">
         <span>
-            <p className="font-weight-bold h3">Technologies</p>
-            <p><span className="font-weight-bold">I am proficient with:</span><br/>
+            <p className="fw-bold h3">Technologies</p>
+            <p><span className="fw-bold">I am proficient with:</span><br/>
                         Java 8+, JUnit 4</p>
-            <p><span className="font-weight-bold">I know fine:</span><br/>
+            <p><span className="fw-bold">I know fine:</span><br/>
                         Git, Python 3, Postman, SQL</p>
-            <p><span className="font-weight-bold">I am familiar with:</span><br/>
+            <p><span className="fw-bold">I am familiar with:</span><br/>
                         Node.js, HTML, CSS, Android, C#, NUnit, JMeter</p>
-            <p><span className="font-weight-bold">I met:</span><br/>
+            <p><span className="fw-bold">I met:</span><br/>
                         Kotlin, Selenium, Protractor</p>
-            <p><span className="font-weight-bold">I wish to know:</span><br/>
+            <p><span className="fw-bold">I wish to know:</span><br/>
                         Go, Java Spring</p>
         </span>
     </div>
